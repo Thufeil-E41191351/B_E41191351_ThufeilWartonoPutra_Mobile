815ca5b70b6edbd4b1db666b2f2bccd304878292
Thufeil-E41191351 <thufeilwartonoputra@gmail.com>
2021-04-19 14:03:39 +0700

    Summary

---
+++ Intent/Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button explicitintent;
+    Button implicitintent;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        explicitintent = (Button)findViewById(R.id.explicit);
+        explicitintent.setOnClickListener(this);
+        implicitintent = (Button)findViewById(R.id.implicit);
+        implicitintent.setOnClickListener(this);
+    }
+
+    @Override public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.explicit:
+                Intent explicit = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(explicit);
+                break;
+            case R.id.implicit:
+                Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/"));
+                startActivity(implicit);
+                break;
+            default:
+                break;
+        }
+    }
+}
