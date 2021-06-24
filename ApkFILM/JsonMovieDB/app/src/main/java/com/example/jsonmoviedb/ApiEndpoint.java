9cc26d65e39abfc1b8194110ece12365a5535f65
Thufeil-E41191351 <thufeilwartonoputra@gmail.com>
2021-06-24 16:21:47 +0700

    Summary

---
+++ ApkFILM/JsonMovieDB/app/src/main/java/com/example/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,12 @@
+package com.example.jsonmoviedb;
+
+public class ApiEndpoint {
+    public static String BASEURL = "http://85.114.138.56/country/indonesia/";
+    public static String APIKEY = "api_key=f1ea9cc8885208c3187615f4e6d0a320";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
